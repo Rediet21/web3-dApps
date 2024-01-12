783c8d3f43e0b3a54953672deaf6a0dbadf14076
Rediet21 <redogirma@gmail.com>
2024-01-13 02:38:36 +0300

    Frontend Login Page

--- Frontend_dApps/src/App.js
+++ Frontend_dApps/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import { loadStdlib } from '@reach-sh/stdlib'
+import MyAlgoConnect from '@reach-sh/stdlib/ALGO_MyAlgoConnect';
+import React from 'react'
 import './App.css';
+
+import Login from './components/Login'
+import Trainee from './pages/Trainee'
+import ProtectedRoute from './components/ProtectedRoute'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import './assets/css/app.css'
+import Home from './pages/Home'
+import Trainer from './pages/Trainer';
+require('dotenv').config()
+
+const reach = loadStdlib("ALGO")
+reach.setWalletFallback(reach.walletFallback({
+    providerEnv: 'TestNet', MyAlgoConnect
+}));
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} exact element={<Home />} />
+                    <Route path={"/login"} exact element={<Login />} />
+
+                    <Route exact path='/trainee' element={<ProtectedRoute />}>
+                        <Route exact path='/trainee' element={<Trainee />} />
+                    </Route>
+                    <Route exact path='/trainer' element={<ProtectedRoute />}>
+                        <Route exact path='/trainer' element={<Trainer />} />
+                    </Route>
+        
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
